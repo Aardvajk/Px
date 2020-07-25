cccb7eb0928d647571acc5f080dfd1bc0587ac5d
Aardvajk <aardvajk@live.co.uk>
2020-07-25 11:33:52 +0100

    Update readme

--- Pc/parser/Parser.cpp
+++ Pc/parser/Parser.cpp
@@ -1,11 +1,35 @@
 #include "Parser.h"
 
+#include "framework/Error.h"
+
+#include "application/Context.h"
+
 #include "nodes/BlockNode.h"
+
+namespace
+{
+
+void construct(Context &c, BlockNode *block, bool get)
+{
+    auto tok = c.scanner.next(get);
+    switch(tok.type())
+    {
+        default: throw Error(tok.location(), "construct expected - ", tok.text());
+    }
+}
+
+}
 
 NodePtr Parser::build(Context &c)
 {
     auto block = new BlockNode({ });
     NodePtr n(block);
+
+    c.scanner.next(true);
+    while(c.scanner.token().type() != Token::Type::Eof)
+    {
+        construct(c, block, false);
+    }
 
     return n;
 }
