f6375f4b7e89aaf5676d14f81411e9bf2dd00349
Aardvajk <aardvajk@live.co.uk>
2021-03-17 12:29:51 +0000

    Pc added

---
+++ Pc/main.cpp
@@ -0,0 +1,35 @@
+#include "framework/Error.h"
+
+#include "application/Context.h"
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::vector<std::string> files;
+    Context c(argc, argv, files);
+
+    try
+    {
+        if(files.size() < 1)
+        {
+            throw Error("no source specified");
+        }
+
+        c.open(files[0]);
+    }
+
+    catch(const Error &e)
+    {
+        std::cerr << "pc error";
+
+        if(auto n = e.location())
+        {
+            std::cerr << " " << c.sources.path(n.id()) << " " << n.line() << "," << n.column();
+        }
+
+        std::cerr << ": " << e.what() << "\n";
+
+        return -1;
+    }
+}
--- Px/scanner/Lexer.h
+++ Px/scanner/Lexer.h
@@ -11,6 +11,7 @@ namespace Lexer
 
 enum class Mode
 {
+    Pc,
     Pi
 };
 
